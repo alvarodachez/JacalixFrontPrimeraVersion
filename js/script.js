28b6657a9d3333911450da6507f19e15336592ba
alvarodachez <sanchez99esp@gmail.com>
2020-11-01 18:13:32 +0100

    añadidas visualizaciones al perfil

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const signButton = document.getElementById("signButton");
 const logButton = document.getElementById("logButton");
+const editProfileButton = document.getElementById("editProfileButton");
 let userActive;
+let views;
 
 
 const completeProfileRequest = () =>{
@@ -19,8 +21,30 @@ const reHome = (name) =>{
     sessionStorage.setItem('nameActive',name);
     window.open("home.html","_self");
 }
+const completeViews = (v) =>{
+    let paren = document.getElementById("colViews");
+    let viewsList = document.getElementById("viewsList");
+    let newViewsList = document.createElement("ul");
+    newViewsList.setAttribute("class","list-group list-group-flush")
+
+    if(v != []){
+        for(i of v){
+            let elemento = document.createElement("li");
+            elemento.setAttribute("class","list-group-item notranslate")
+            let texto = document.createTextNode("Name: "+i.productViewed.name+" | Category: "+i.productViewed.cat+" | Subscription: "+i.productViewed.rent);
+            elemento.appendChild(texto);
+            newViewsList.appendChild(elemento);
+        }
+        paren.replaceChild(newViewsList,viewsList);
+    }
+
+
+    
+}
 const completeProfile = (res) =>{
-    console.log(res);
+    userActive = res.id;
+    views = res.views;
+    //console.log(views[0].productViewed)
     let profileName = document.getElementById("profileName");
     let profileSurname = document.getElementById("profileSurname");
     let profileAge = document.getElementById("profileAge");
@@ -31,10 +55,56 @@ const completeProfile = (res) =>{
     profileSurname.innerText = "Surname; "+ res.surname;
     profileAge.innerText = "Age: " + res.age;
     profileDni.innerText = "Dni: " + res.dni;
-    profileSubscription.innerText = "Subscription: "+ res.sub;
+    profileSubscription.innerText = "Subscription: "+ res.sub.rentType;
+    completeViews(views);
 }
 if(document.location == 'file:///home/alvaro/Escritorio/JacalixFrontPrimeraVersion/home.html'){
     completeProfileRequest();
+
+    editProfileButton.addEventListener("click", () => {
+        let editName = document.getElementById("editName").value;
+        let editSurname = document.getElementById("editSurname").value;
+        let editAge = document.getElementById("editAge").value;
+        let editDni = document.getElementById("editDni").value;
+        let editSubscription = document.getElementById("editSubscription").value;
+
+        const user = {
+            name : editName,
+            surname : editSurname,
+            age : editAge,
+            dni : editDni
+        }
+
+        const sub = {
+            rentType : editSubscription,
+            startSuscription : "2020-10-06",
+            endSuscription : "2020-10-06",
+            price : 10
+        }
+
+        fetch('http://localhost:8080/jacalix/customers/subs/'+userActive,{
+            method: 'PUT',
+            body: JSON.stringify(sub),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+        .then(response => response.json())
+
+        fetch('http://localhost:8080/jacalix/customers/'+parseInt(userActive,10),{
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+        .then(response => response.json())
+
+        
+        
+
+        location.reload();
+    })
 }
 if(document.location == 'file:///home/alvaro/Escritorio/JacalixFrontPrimeraVersion/index.html'){
 
